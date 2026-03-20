70884d20e26e3079b27cacc916b21ac8a76adcfd
fernandinha4k <raquely.gosmati@escola.pr.gov.br>
2026-03-20 07:53:39 -0300

    update

--- aulas/19032026.js
+++ aulas/19032026.js
@@ -5,5 +5,26 @@
 //Números
 let numero = 30
 let dacimal = 20.30
-// String
-let  nome = "Não sei, vocễ não sabe?"
+
+//String
+let nome = "Não sei, vocễ não sabe?"
+let nome1 = "NÂO SEI, VOCÊ NÃO SABE?"
+
+console.log(nome === nome1); // false
+
+const inputMinusculo = nome1.toLowerCase();
+
+console.log(nome === inputMinusculo); // true
+
+const senha = "minhaSenha123"
+console.log(senha.length) // 13 caracteres
+
+
+// Booleanos
+
+let nv = true;
+let nz = false;
+
+if(nv!==nz){
+    console.log('nv é diferente, pois ele é ${nv}')
+}

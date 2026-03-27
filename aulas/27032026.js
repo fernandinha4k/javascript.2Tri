eb9a4945cdeab414f1f242f2f713061b00e2fb5f
fernandinha4k <raquely.gosmati@escola.pr.gov.br>
2026-03-27 07:56:55 -0300

    update

---
+++ aulas/27032026.js
@@ -0,0 +1,15 @@
+// Criar Array
+
+const fila = [1,2,3,4,5,6];
+
+const filaStringAmigos = ["Luiza","Larissa","Renan","bob"]
+
+function lerAmigos(filaStringAmigos){
+    console.log(`Você tem ${filaStringAmigos.length}amigos`)
+   
+    for(let i = 0; i < filaStringAmigos.length;i++){
+        console.log(filaStringAmigos[i])
+        if(filaStringAmigos[i] === "bob)"{
+            console.log(`index do seu melhor amigo é $()`)
+    }
+}
